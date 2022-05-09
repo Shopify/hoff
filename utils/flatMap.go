a2e75869e4d82d0aa26948eca67e028707bd2e95
Paco Castro <paco.castro@shopify.com>
2022-05-09 21:53:07 +0200

    Document and add tests for FlatMap

--- utils/flatMap.go
+++ utils/flatMap.go
@@ -2,6 +2,9 @@ package utils
 
 import "context"
 
+// FlatMap applies a transformation to an array of elements and
+// returns another array with the transformed result
+// Example: FlatMap([]string{"abcd", "efg"}, func(s string) []int { return []int{len(s)} }) = [4, 3].
 func FlatMap[In, Out any](arr []In, fn func(In) []Out) (out []Out) {
 	for _, elem := range arr {
 		out = append(out, fn(elem)...)
@@ -9,6 +12,8 @@ func FlatMap[In, Out any](arr []In, fn func(In) []Out) (out []Out) {
 	return out
 }
 
+// FlatMapContext applies the FlatMap transformation while, at the same time,
+// shares a context with the transforming function.
 func FlatMapContext[In, Out any](ctx context.Context, arr []In, fn func(context.Context, In) []Out) (out []Out) {
 	for _, elem := range arr {
 		out = append(out, fn(ctx, elem)...)
--- utils/flatMap_test.go
+++ utils/flatMap_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"context"
 	"strings"
 	"testing"
 
@@ -8,23 +9,31 @@ import (
 )
 
 type FlatMapTestCase struct {
-	In  []string
-	Out []string
-	Fn  func(s string) []string
+	In    []string
+	Out   []string
+	Fn    func(s string) []string
+	FnCtx func(ctx context.Context, s string) []string
 }
 
 var testCases = []FlatMapTestCase{
 	{
-		In:  []string{"abcd"},
-		Out: []string{"a", "b", "c", "d"},
-		Fn:  splitString,
+		In:    []string{"abcd"},
+		Out:   []string{"a", "b", "c", "d"},
+		Fn:    splitString,
+		FnCtx: splitStringWithContext,
 	},
 	{
-		In:  []string{"abcd", "efg"},
-		Out: []string{"a", "b", "c", "d", "e", "f", "g"},
-		Fn:  splitString,
+		In:    []string{"abcd", "efg"},
+		Out:   []string{"a", "b", "c", "d", "e", "f", "g"},
+		Fn:    splitString,
+		FnCtx: splitStringWithContext,
 	},
 }
+
+type contextKey string
+
+var key = contextKey("key")
+var testKey = contextKey("T")
 
 func TestFlatMap(t *testing.T) {
 	for _, testCase := range testCases {
@@ -33,6 +42,24 @@ func TestFlatMap(t *testing.T) {
 	}
 }
 
+func TestFlatMapContext(t *testing.T) {
+	ctx := context.WithValue(context.Background(), key, "value")
+	ctx = context.WithValue(ctx, testKey, t)
+
+	for _, testCase := range testCases {
+		out := FlatMapContext(ctx, testCase.In, testCase.FnCtx)
+		require.Equal(t, testCase.Out, out)
+		require.Equal(t, "value", ctx.Value(key))
+	}
+}
+
 func splitString(s string) []string {
+	return strings.Split(s, "")
+}
+
+func splitStringWithContext(ctx context.Context, s string) []string {
+	t := ctx.Value(testKey).(*testing.T)
+	require.Equal(t, "value", ctx.Value(key))
+
 	return strings.Split(s, "")
 }
