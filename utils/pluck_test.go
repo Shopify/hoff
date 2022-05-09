eb11975a73c6ff05358b0158b5ca9017a9b27c84
Jade Ornelas <jade.ornelas@shopify.com>
2022-05-09 16:25:21 -0700

    feat: create pluck function

---
+++ utils/pluck_test.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPluckStringBasic(t *testing.T) {
+	output := Pluck([]map[string]any{
+		{"foo": 1, "bar": 2, "shop": 3},
+		{"foo": 4, "bar": 5, "shop": 6},
+	}, "foo", "bar")
+	require.Equal(t, [][]any{{1, 2}, {4, 5}}, output, "2 keys extracted")
+}
+
+func TestPluckStringNil(t *testing.T) {
+	output := Pluck([]map[string]any{
+		{"foo": 1, "bar": 2, "shop": 3},
+		{"foo": 4, "bar": 5},
+	}, "foo", "shop")
+	require.Equal(t, [][]any{{1, 3}, {4, nil}}, output, "2 keys extracted with nil value")
+}
+
+func TestPluckStringSingleKey(t *testing.T) {
+	output := Pluck([]map[string]any{
+		{"foo": 1, "bar": 2, "shop": 3},
+		{"foo": 4, "bar": 5},
+	}, "foo")
+	require.Equal(t, [][]any{{1}, {4}}, output, "1 keys extracted")
+}
