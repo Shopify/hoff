9c5bf339f7f719c09ab20410f6959e9605a66d07
Chris Pappas <chris.pappas@shopify.com>
2022-05-09 14:32:03 -0500

    Add FilterContext and FilterContextError

--- utils/filter.go
+++ utils/filter.go
@@ -1,5 +1,7 @@
 package utils
 
+import "context"
+
 // Filter takes an array of T's and applies the callback fn to each element.
 // If fn returns true, the element is included in the returned collection, if false it is excluded.
 // Example: [1, 2, 3].Filter(<number is odd?>) = [1, 3].
@@ -10,4 +12,34 @@ func Filter[T any](arr []T, fn func(T) bool) (out []T) {
 		}
 	}
 	return out
+}
+
+func FilterContext[T any](
+	ctx context.Context,
+	arr []T,
+	fn func(ctx context.Context, elem T) bool,
+) (out []T) {
+	for _, elem := range arr {
+		if fn(ctx, elem) {
+			out = append(out, elem)
+		}
+	}
+	return out
+}
+
+func FilterContextError[T any](
+	ctx context.Context,
+	arr []T,
+	fn func(ctx context.Context, elem T) (bool, error),
+) (out []T, err error) {
+	for _, elem := range arr {
+		include, err := fn(ctx, elem)
+		if err != nil {
+			return nil, err
+		}
+		if include {
+			out = append(out, elem)
+		}
+	}
+	return out, nil
 }
--- utils/filter_test.go
+++ utils/filter_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -43,4 +45,55 @@ func TestFilterStructs(t *testing.T) {
 		},
 	)
 	require.Len(t, filtered, 2)
+}
+
+func contextAwareCallbackInt(ctx context.Context, i int) bool {
+	// check whether ctx's value for "foo" is "bar" and the number is odd
+	return ctx.Value("foo") == "bar" && i%2 == 1
+}
+
+func TestFilterContext(t *testing.T) {
+	ctx := context.WithValue(context.Background(), "foo", "bar")
+	ints := []int{1, 2, 3}
+	filtered := FilterContext(ctx, ints, contextAwareCallbackInt)
+	require.ElementsMatch(t, filtered, []int{1, 3})
+
+	ctx2 := context.Background()
+	filtered = FilterContext(ctx2, ints, contextAwareCallbackInt)
+	// should not match anything since the context doesn't have a value for "foo"
+	require.Len(t, filtered, 0)
+
+	ctx3 := context.WithValue(context.Background(), "foo", "nada")
+	filtered = FilterContext(ctx3, ints, contextAwareCallbackInt)
+	// should not match anything since the context "foo" value is not "bar"
+	require.Len(t, filtered, 0)
+}
+
+func contextErrorAwareCallbackInt(ctx context.Context, i int) (bool, error) {
+	if ctx.Value("throwError") == true {
+		return false, fmt.Errorf("throwError true")
+	}
+	// check whether ctx's value for "foo" is "bar" and the number is odd
+	return ctx.Value("foo") == "bar" && i%2 == 1, nil
+}
+
+func TestFilterContextError(t *testing.T) {
+	ctx := context.WithValue(context.Background(), "foo", "bar")
+	ints := []int{1, 2, 3}
+	filtered, err := FilterContextError(ctx, ints, contextErrorAwareCallbackInt)
+	require.NoError(t, err)
+	require.ElementsMatch(t, filtered, []int{1, 3})
+
+	ctx2 := context.Background()
+	filtered, err = FilterContextError(ctx2, ints, contextErrorAwareCallbackInt)
+	// should not match anything since the context doesn't have a value for "foo"
+	require.NoError(t, err)
+	require.Len(t, filtered, 0)
+
+	// create a context with foo = bar AND throwError = true
+	ctx3 := context.WithValue(context.Background(), "throwError", true)
+	filtered, err = FilterContextError(ctx3, ints, contextErrorAwareCallbackInt)
+	// should throw an error
+	require.Error(t, err)
+	require.Nil(t, filtered)
 }
