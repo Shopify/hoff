f420bede977453f3cea04b626481c03636d247ac
Chris Langager <christopher.langager@gmail.com>
2022-05-10 09:50:58 -0600

    delete things that are specific to shopify-monitoring-api or are a bit too hacky to live here

--- utils/mapToField.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func MapToStringField[T any](tt []T, prop string) []string {
-	out := make([]string, 0, len(tt))
-	for _, elem := range tt {
-		out = append(out, GetStringField(elem, prop))
-	}
-	return out
-}
--- utils/notDefaultValue.go
+++
@@ -1,6 +0,0 @@
-package utils
-
-func NotDefaultValue[T comparable](t T) bool {
-	var defaultValue T
-	return t != defaultValue
-}
