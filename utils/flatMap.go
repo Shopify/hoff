4d45fd350b4d30b313859cc8508ddf228632b6b1
Paco Castro <paco.castro@shopify.com>
2022-05-10 19:39:07 +0200

    Add FlatMapError functions, refactor tests

--- utils/flatMap.go
+++ utils/flatMap.go
@@ -11,6 +11,20 @@ func FlatMap[In, Out any](arr []In, fn func(In) []Out) (out []Out) {
 	return out
 }
 
+// FlatMap applies a transformation to an array of elements and
+// returns another array with the transformed result. If one of the
+// transformations fails, it will return early
+func FlatMapError[In, Out any](arr []In, fn func(In) ([]Out, error)) (out []Out, err error) {
+	for _, elem := range arr {
+		tr, err := fn(elem)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, tr...)
+	}
+	return out, nil
+}
+
 // FlatMapContext applies the FlatMap transformation while, at the same time,
 // shares a context with the transforming function.
 func FlatMapContext[In, Out any](ctx context.Context, arr []In, fn func(context.Context, In) []Out) (out []Out) {
@@ -18,4 +32,18 @@ func FlatMapContext[In, Out any](ctx context.Context, arr []In, fn func(context.
 		out = append(out, fn(ctx, elem)...)
 	}
 	return out
+}
+
+// FlatMapContext applies the FlatMap transformation while, at the same time,
+// shares a context with the transforming function. If one of the
+// transformations fails, it will return early
+func FlatMapContextError[In, Out any](ctx context.Context, arr []In, fn func(context.Context, In) ([]Out, error)) (out []Out, err error) {
+	for _, elem := range arr {
+		tr, err := fn(ctx, elem)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, tr...)
+	}
+	return out, nil
 }
--- utils/flatMap_test.go
+++ utils/flatMap_test.go
@@ -28,7 +28,6 @@ var testCases = []flatMapTestCase{
 type contextKey string
 
 var key = contextKey("key")
-var testKey = contextKey("T")
 
 func TestFlatMap(t *testing.T) {
 	for _, testCase := range testCases {
@@ -37,21 +36,130 @@ func TestFlatMap(t *testing.T) {
 	}
 }
 
-func splitString(s string) []string {
-	return strings.Split(s, "")
-}
-
 func ExampleFlatMap() {
 	fmt.Println(FlatMap([]string{"abcd", "efg"}, func(s string) []int { return []int{len(s)} }))
 	// Output: [4 3]
 }
 
 var benchmarkData = createStringSlice(100, 1000)
+var benchmarkResult []string
+var benchmarkError error
 
 func BenchmarkFlatMap(b *testing.B) {
+	var r []string
 	for i := 0; i < b.N; i++ {
-		FlatMap(benchmarkData, splitString)
+		r = FlatMap(benchmarkData, splitString)
 	}
+	benchmarkResult = r
+}
+
+func TestFlatMapErrorSucceeds(t *testing.T) {
+	out, err := FlatMapError(testCases[0].in, splitStringWithError)
+
+	require.NoError(t, err)
+	require.Equal(t, testCases[0].out, out)
+}
+
+func TestFlatMapErrorFails(t *testing.T) {
+	out, err := FlatMapError(testCases[1].in, splitStringWithError)
+
+	require.Error(t, err)
+	require.Len(t, out, 0)
+}
+
+func ExampleFlatMapError() {
+	properPrefixes := func(s string) ([]string, error) {
+		if len(s) < 2 {
+			return nil, fmt.Errorf("String '%s' has no proper prefixes", s)
+		}
+
+		res := make([]string, len(s)-1)
+		for i := 1; i < len(s); i++ {
+			res[i-1] = s[0:i]
+		}
+
+		return res, nil
+	}
+
+	var err error
+	prefixes, _ := FlatMapError([]string{"abcd", "efg"}, properPrefixes)
+	fmt.Println(prefixes)
+	prefixes, err = FlatMapError([]string{"abcd", "x", "efg"}, properPrefixes)
+	fmt.Println(err)
+	// Output:
+	// [a ab abc e ef]
+	// String 'x' has no proper prefixes
+}
+
+func BenchmarkFlatMapError(b *testing.B) {
+	var r []string
+	var err error
+	for i := 0; i < b.N; i++ {
+		r, err = FlatMapError(benchmarkData, splitStringWithError)
+	}
+	benchmarkResult = r
+	benchmarkError = err
+}
+
+func TestFlatMapContext(t *testing.T) {
+	fn := func(c context.Context, s string) []string {
+		require.Equal(t, "a_value", c.Value(key))
+
+		return splitString(s)
+	}
+	ctx := context.WithValue(context.Background(), key, "a_value")
+
+	for _, testCase := range testCases {
+		out := FlatMapContext(ctx, testCase.in, fn)
+		require.Equal(t, testCase.out, out)
+		require.Equal(t, "a_value", ctx.Value(key))
+	}
+}
+
+func TestFlatMapContextErrorSucceeds(t *testing.T) {
+	fn := func(c context.Context, s string) ([]string, error) {
+		require.Equal(t, "a_value", c.Value(key))
+
+		return splitStringWithContextAndError(c, s)
+	}
+	ctx := context.WithValue(context.Background(), key, "a_value")
+
+	out, err := FlatMapContextError(ctx, testCases[0].in, fn)
+	require.NoError(t, err)
+	require.Equal(t, testCases[0].out, out)
+	require.Equal(t, "a_value", ctx.Value(key))
+}
+
+func TestFlatMapContextErrorFails(t *testing.T) {
+	fn := func(c context.Context, s string) ([]string, error) {
+		require.Equal(t, "a_value", c.Value(key))
+
+		return splitStringWithContextAndError(c, s)
+	}
+	ctx := context.WithValue(context.Background(), key, "a_value")
+
+	out, err := FlatMapContextError(ctx, testCases[1].in, fn)
+	require.Error(t, err)
+	require.Len(t, out, 0)
+	require.Equal(t, "a_value", ctx.Value(key))
+}
+
+func splitString(s string) []string {
+	return strings.Split(s, "")
+}
+
+func splitStringWithError(s string) ([]string, error) {
+	if len(s) < 4 {
+		return nil, fmt.Errorf("String '%s' too short", s)
+	}
+	return strings.Split(s, ""), nil
+}
+
+func splitStringWithContextAndError(ctx context.Context, s string) ([]string, error) {
+	if len(s) < 4 {
+		return nil, fmt.Errorf("String '%s' too short", s)
+	}
+	return strings.Split(s, ""), nil
 }
 
 func createStringSlice(length, num int) []string {
@@ -63,19 +171,4 @@ func createStringSlice(length, num int) []string {
 	}
 
 	return data
-}
-
-func TestFlatMapContext(t *testing.T) {
-	splitStringWithContext := func(c context.Context, s string) []string {
-		require.Equal(t, "a_value", c.Value(key))
-
-		return strings.Split(s, "")
-	}
-	ctx := context.WithValue(context.Background(), key, "a_value")
-
-	for _, testCase := range testCases {
-		out := FlatMapContext(ctx, testCase.in, splitStringWithContext)
-		require.Equal(t, testCase.out, out)
-		require.Equal(t, "a_value", ctx.Value(key))
-	}
 }
