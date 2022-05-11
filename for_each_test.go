95f21f81de46771dea51669023b7648f99b8507b
Jade Ornelas <jade.ornelas@shopify.com>
2022-05-11 12:28:23 -0700

    test(for_each): run success and error cases together

--- for_each_test.go
+++ for_each_test.go
@@ -39,7 +39,6 @@ func TestForEach(t *testing.T) {
 }
 
 func TestForEachContext(t *testing.T) {
-	type contextKey string
 	var key = contextKey("key")
 
 	for _, testCase := range forEachTestCases {
@@ -58,37 +57,39 @@ func TestForEachContext(t *testing.T) {
 }
 
 func TestForEachContextError(t *testing.T) {
-	type contextKey string
 	var key = contextKey("key")
 	input := []string{"aaa", "bbb"}
 
-	var calledInputs = make([]string, 0, len(input))
-	fn := func(c context.Context, s string) error {
-		require.Equal(t, "a_value", c.Value(key))
-		calledInputs = append(calledInputs, s)
-		return nil
-	}
-	ctx := context.WithValue(context.Background(), key, "a_value")
+	t.Run(
+		"success", func(t *testing.T) {
+			var calledInputs = make([]string, 0, len(input))
+			fn := func(c context.Context, s string) error {
+				require.Equal(t, "a_value", c.Value(key))
+				calledInputs = append(calledInputs, s)
+				return nil
+			}
+			ctx := context.WithValue(context.Background(), key, "a_value")
 
-	err := ForEachContextError(ctx, input, fn)
-	require.Nil(t, err)
-	require.Equal(t, input, calledInputs)
-}
+			err := ForEachContextError(ctx, input, fn)
+			require.Nil(t, err)
+			require.Equal(t, input, calledInputs)
+		},
+	)
 
-func TestForEachContextErrorWithError(t *testing.T) {
-	type contextKey string
-	var key = contextKey("key")
-	input := []string{"aaa", "bbb"}
+	t.Run(
+		"failure", func(t *testing.T) {
+			var calledInputs = make([]string, 0, len(input))
+			fn := func(c context.Context, s string) error {
+				require.Equal(t, "a_value", c.Value(key))
+				calledInputs = append(calledInputs, s)
+				return errors.New("catastrophic error")
+			}
+			ctx := context.WithValue(context.Background(), key, "a_value")
 
-	var calledInputs = make([]string, 0, len(input))
-	fn := func(c context.Context, s string) error {
-		require.Equal(t, "a_value", c.Value(key))
-		calledInputs = append(calledInputs, s)
-		return errors.New("catastrophic error")
-	}
-	ctx := context.WithValue(context.Background(), key, "a_value")
+			err := ForEachContextError(ctx, input, fn)
+			require.ErrorContains(t, err, "catastrophic error")
+			require.Equal(t, input[0:1], calledInputs) // only first fn was called beacuse it return error
+		},
+	)
 
-	err := ForEachContextError(ctx, input, fn)
-	require.ErrorContains(t, err, "catastrophic error")
-	require.Equal(t, input[0:1], calledInputs) // only first fn was called beacuse it return error
 }
