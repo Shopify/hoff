20d598f22fdcabc9430d5d7a865bda18fe142654
Eduardo Vicente Gonçalves <eduardo.vicentegoncalves@shopify.com>
2022-09-19 12:10:46 -0400

    Makes Chunk's examples to runnable

--- chunk.go
+++ chunk.go
@@ -3,10 +3,6 @@ package hoff
 // Chunk takes an input array of T elements, and "chunks" into groups of chunkSize elements.
 // If the input array is empty, or the batchSize is 0, return an empty slice of slices.
 // Adapted to generics from https://github.com/golang/go/wiki/SliceTricks#batching-with-minimal-allocation.
-// Examples:
-// Chunk([]int{1, 2, 3, 4, 5}, 2) = [[1, 2] [3, 4], [5]]
-// Chunk([]int{}, 2) = []
-// Chunk([]int{1, 2, 3}, 0) = [].
 func Chunk[T any](actions []T, batchSize int) [][]T {
 	// if the input is empty or batch size is 0, return an empty slice of slices
 	if len(actions) == 0 || batchSize < 1 {
